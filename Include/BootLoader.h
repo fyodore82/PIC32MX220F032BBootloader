b306a37c500be4559da0dd5b87486b15673950cd
fyodore82 <fyodor_e@hotmail.com>
2022-05-13 09:32:36 +1000

    Bootloader builds and run

--- Include/BootLoader.h
+++ Include/BootLoader.h
@@ -41,15 +41,14 @@
  		 
  		2)The base address and end address must align on  4K address boundary */
  		
-#define APP_FLASH_BASE_ADDRESS 	0x9D000000 
+#define APP_FLASH_BASE_ADDRESS 	0x9D002600 
 #define APP_FLASH_END_ADDRESS   PROGRAM_FLASH_END_ADRESS
 
 /* Address of  the Flash from where the application starts executing */
 /* Rule: Set APP_FLASH_BASE_ADDRESS to _RESET_ADDR value of application linker script*/
 
 // For PIC32MX1xx and PIC32MX2xx Controllers only
-// ToDo: check!
-#define USER_APP_RESET_ADDRESS 	(0x9D000000 + 0x0)
+#define USER_APP_RESET_ADDRESS 	(0x9D002600 + 0x0)
 
 
 
